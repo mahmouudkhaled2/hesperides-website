074d10c9d02383c2a404ebc6910d0798bc09bf97
Mahmouud Khaled <147172075+mahmouudkhaled2@users.noreply.github.com>
2025-03-14 00:30:18 +0200

    Update middleware.ts sec

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['ar', 'en'],
   defaultLocale: 'ar',
+  localeDetection: false,
 });
 
 export const config = {
-  matcher: '/:path*',
+  matcher: '/((?!api|_next|.*\\..*).*)',
 };
